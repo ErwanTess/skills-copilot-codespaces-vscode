b3179f826b8b2fe09ff66df25b625109bccd574c
ErwanTess <165020724+ErwanTess@users.noreply.github.com>
2024-03-27 16:51:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    return {
+        // Path: member.js
+        getSkills: function() {
+            return ['Node.js', 'React', 'Angular', 'Vue', 'Ember', 'Backbone'];
+        }
+    }
+}
